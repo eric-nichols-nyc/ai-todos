4c543df9b76c13157a059fb1b26e0ef720976b19
Eric Nichols <ebn646@hotmail.com>
2024-08-26 21:02:10 -0400

    added data to public follder

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 import fs from 'fs/promises';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const tasksFilePath = './tasks.json';
+const tasksFilePath = 'public/data/tasks.json';
 
 async function readTasks() {
   const data = await fs.readFile(tasksFilePath, 'utf8');
