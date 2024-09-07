7c295b0d37e176cbb456ef59242ea32dfc8984a4
Eric Nichols <ebn646@hotmail.com>
2024-09-07 18:24:52 -0400

    adjust the task type

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,7 @@ export type Task = {
     id: string;
     task: string;
     priority: 'high' | 'medium' | 'low';
-    due_date: string | null;
+    due_date?: string | null;
     created_at: string;
     updated_at?: string;
     completed: boolean;
