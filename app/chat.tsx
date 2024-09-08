845789cd44822c8524f8bc399ee288fbdb692ad4
Eric Nichols <ebn646@hotmail.com>
2024-09-08 14:43:08 -0400

    feat: Add initial message and avatar to chat component

--- app/chat.tsx
+++ app/chat.tsx
@@ -193,8 +193,21 @@ export const Chat = () => {
   const getMessageVariant = (role: string) => role === "ai" ? "received" : "sent";
 
   return (
-    <div className="h-[calc(100dvh)] flex flex-col">
+    <div className="h-full flex flex-col">
       <div className="flex-grow overflow-y-auto bg-red-300">
+            {/* Initial Message */}
+            {messages.length === 0 && (
+          <div className='w-full bg-background shadow-sm border rounded-lg p-8 flex flex-col gap-2'>
+            <h1 className='font-bold'>Welcome to this example app.</h1>
+            <p className='text-muted-foreground text-sm'>This is a simple Next.JS example application created using <a href="https://github.com/jakobhoeg/shadcn-chat" className='font-bold inline-flex flex-1 justify-center gap-1 leading-4 hover:underline'>shadcn-chat
+              <svg aria-hidden="true" height="7" viewBox="0 0 6 6" width="7" className="opacity-70"><path d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z" fill="currentColor"></path></svg>
+            </a> components. It uses <a href="https://sdk.vercel.ai/" className='font-bold inline-flex flex-1 justify-center gap-1 leading-4 hover:underline'>Vercel AI SDK
+                <svg aria-hidden="true" height="7" viewBox="0 0 6 6" width="7" className="opacity-70"><path d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z" fill="currentColor"></path></svg>
+              </a> for the AI integration. Build chat interfaces like this at lightspeed with shadcn and shadcn-chat.</p>
+            <p className='text-muted-foreground text-sm'>Make sure to also checkout the shadcn-chat support component at the bottom right corner.</p>
+          </div>
+        )}
+
         <ChatMessageList ref={messagesContainerRef}>
           <AnimatePresence>
             {messages.map((message, index) => {
@@ -220,7 +233,7 @@ export const Chat = () => {
                   <ChatBubble key={index} variant={variant}>
                     <Avatar>
                       <AvatarImage
-                        src=''
+                        src='/avatar.jpeg'
                         alt="Avatar"
                         className={message.role === "ai" ? "dark:invert" : ""}
                       />
@@ -263,7 +276,7 @@ export const Chat = () => {
       <form
         ref={formRef}
         onSubmit={onSubmit}
-        className="relative rounded-lg h-[86px] border bg-background focus-within:ring-1 focus-within:ring-ring flex-shrink-0 mb-5"
+        className="relative rounded-lg h-[86px] border bg-background focus-within:ring-1 focus-within:ring-ring flex-shrink-0"
       >
         <ChatInput
           ref={inputRef}
